5c495c84405948ed9faca46991d60c8f4648b4d8
Opal Harrison <ornrumpa.km@gmail.com>
2021-09-10 19:07:47 -0700

    Revert and move the clearing to solve reference
    this is cleaner and less error proned

--- public/app.js
+++ public/app.js
@@ -142,26 +142,23 @@ $(document).on("click", ".body", function () {
 $(document).on("click", "#savenote", function () {
   // Grab the id associated with the article from the submit button
   var thisId = $(this).attr("data-id");
-  var notesSection = $('#notes');
-  var title = notesSection.children('#titleinput')[0].value;
-  var body = notesSection.children('#bodyinput')[0].value;
-
-  $("#notes").html("Notes saved, click the article to view");
   // Run a POST request to change the note, using what's entered in the inputs
   $.ajax({
     method: "POST",
     url: "/articles/" + thisId,
     data: {
       // Value taken from title input
-      title: title,
+      title: $("#titleinput").val(),
       // Value taken from note textarea
-      body: body
+      body: $("#bodyinput").val()
     }
   })
 
     // With that done
     .then(function (data) {
       // Log the response
+      $("#notes").html("Notes saved, click the article to view");
+
       // console.log(data);
       // Empty the notes section
       setTimeout(() => {
@@ -173,3 +170,4 @@ $(document).on("click", "#savenote", function () {
   $("#titleinput").val("");
   $("#bodyinput").val("");
 });
+
